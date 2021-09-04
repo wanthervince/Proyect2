3cbbca2328c2edc1c0351b4a4deea811286d8cb7
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-09-04 10:22:17 -0500

    push github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyect2'`, () => {
+  it(`should have as title 'proyect1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyect2');
+    expect(app.title).toEqual('proyect1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proyect2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'proyect1 app is running!'
+    );
   });
 });
